509f7e9d61a56ec3c69788f399d855997478eaee
Jacky Li <jacky.likun@qq.com>
2026-03-08 07:40:25 +0800

    fix(deploy): remove debug logging from console start.sh

--- lakeon-console/start.sh
+++ lakeon-console/start.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
-echo "=== DEBUG: API_URL=${API_URL} ==="
-echo "=== DEBUG: PORT=${PORT} ==="
 envsubst '${PORT} ${API_URL}' < /tmp/default.conf.template > /etc/nginx/conf.d/default.conf
-echo "=== Generated nginx config ==="
-cat /etc/nginx/conf.d/default.conf
-echo "=== End config ==="
 exec nginx -g 'daemon off;'
