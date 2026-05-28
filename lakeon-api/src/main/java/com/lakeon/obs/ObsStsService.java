799b9a87ac1b0777ee73f90ff09ade918d62022b
Jacky Li <jacky.likun@qq.com>
2026-05-28 21:15:20 +0800

    fix(api): mark OBS STS constructor for autowiring

--- lakeon-api/src/main/java/com/lakeon/obs/ObsStsService.java
+++ lakeon-api/src/main/java/com/lakeon/obs/ObsStsService.java
@@ -7,6 +7,7 @@ import com.lakeon.config.LakeonProperties;
 import com.lakeon.hwcloud.HuaweiIamCredentialClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -29,6 +30,7 @@ public class ObsStsService {
     private final HuaweiIamCredentialClient iamCredentialClient;
     private final Cache<String, StsCredentials> cache;
 
+    @Autowired
     public ObsStsService(LakeonProperties props,
                          ObjectMapper objectMapper,
                          HuaweiIamCredentialClient iamCredentialClient) {
