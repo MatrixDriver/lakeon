ce49c8b2056d78772543ba7e0909cc9233f67a56
Jacky Li <jacky.likun@qq.com>
2026-06-25 08:28:27 +0800

    fix(admin): default railway runtime config

--- lakeon-admin/start.sh
+++ lakeon-admin/start.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+: "${PORT:=8080}"
+: "${API_URL:=https://api.dbay.cloud:8443}"
 envsubst '${PORT} ${API_URL}' < /tmp/default.conf.template > /etc/nginx/conf.d/default.conf
 exec nginx -g 'daemon off;'
